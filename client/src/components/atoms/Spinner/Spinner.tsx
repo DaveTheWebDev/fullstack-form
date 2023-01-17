57dde56e0e99f8b84b287cca37ab148586850af7
DaveTheWebDev <dawidantczak2@gmail.com>
2023-01-17 09:10:51 +0100

    chore: added Spinner

---
+++ client/src/components/atoms/Spinner/Spinner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './Spinner.styles'
+
+export const Spinner: React.FC = () => {
+  return (
+    <S.Spinner>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </S.Spinner>
+  )
+}
